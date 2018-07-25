58ab80ea1056e16d3c06d5f0108a2e626af58b3b
Nestor Velazquez Sanchez <netovs@gmail.com>
2018-07-25 11:39:59 -0500

    first commit

--- index.php
+++ index.php
@@ -2,22 +2,21 @@
 <html>
   <head>
     <style>
-       /* Set the size of the div element that contains the map */
       #map {
-        height: 400px;  /* The height is 400 pixels */
-        width: 100%;  /* The width is the width of the web page */
+        height: 480px;  /* The height is 400 pixels */
+        width: 640%;  /* The width is the width of the web page */
        }
     </style>
   </head>
   <body>
-    <h3>My Google Maps Demo</h3>
+    <h3>Google Maps Demo</h3>
     <!--The div element for the map -->
     <div id="map"></div>
     <script>
 // Initialize and add the map
 function initMap() {
   // The location of Uluru
-  var uluru = {lat: -111.6581896, lng: 23.3132908};
+  var uluru = {lat: 23.3132908, lng: -111.6581896};
   // The map, centered at Uluru
   var map = new google.maps.Map(
       document.getElementById('map'), {zoom: 5, center: uluru});
