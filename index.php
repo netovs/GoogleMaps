f283983f7be9b63e935f8b5ac421b6e3212c22dd
Nestor Velazquez Sanchez <netovs@gmail.com>
2018-07-30 11:02:37 -0500

    Cambios en el mapa

--- index.php
+++ index.php
@@ -12,18 +12,50 @@
     <h3>Google Maps Demo</h3>
     <!--The div element for the map -->
     <div id="map"></div>
+	
+	<a href="https://wa.me/?text=Hola,%20me%20interesa%20solicitar%20informes%20para%20pagina%20de%20Internet">Pide informes por Whatsapp</a>
+	
     <script>
 // Initialize and add the map
 function initMap() {
   
-  var centrar = {lat: 21.6660159, lng: -98.6699083};
-  var map = new google.maps.Map(
+	var centrar = {lat: 21.6660159, lng: -98.6699083};
+	var map = new google.maps.Map(
       document.getElementById('map'), {zoom: 5, center: centrar});
   
   
-  // var marker = new google.maps.Marker({position: centrar, map: map});
+	// XHTWEB Plantilla  para PHP
+	var logoXHTWEB	= 'https://xhtweb.com.mx/wp-content/uploads/2016/07/xhtweb_transparente_1.png';
+	var iconXHTWEB = {
+	url: logoXHTWEB, // url
+	scaledSize: new google.maps.Size(100, 100), // scaled size
+	/*
+	origin: new google.maps.Point(0,0), // origin
+	anchor: new google.maps.Point(0, 0) // anchor 
+	*/
+	};
+	var xhtweb = {lat: 20.8698738, lng: -86.9654525};
+	var xhtwebTXT = '<h3>XHTWEB</h3>';
+	var infowindowXHTWEB = new google.maps.InfoWindow({
+		content: xhtwebTXT
+	});
+	var markXHTWEB = new google.maps.Marker({
+	position: xhtweb,
+	map: map,
+	title: 'XHTWEB', 
+	icon: iconXHTWEB
+	});
+	markXHTWEB.addListener('click', function() {
+		infowindowXHTWEB.open(map, markXHTWEB);
+	});
+	
+	
+	
+  
+  
+  
+  
   var logoCWeb 		= 'https://comunicacionweb.com.mx/images/main-logo.png';
-  var logoXHTWEB	= 'https://xhtweb.com.mx/wp-content/uploads/2016/07/xhtweb_transparente_1.png';
   
 var iconCWeb = {
     url: logoCWeb, // url
@@ -33,31 +65,23 @@ var iconCWeb = {
     anchor: new google.maps.Point(0, 0) // anchor*/
 };
 
-var iconXHTWEB = {
-    url: logoXHTWEB, // url
-    scaledSize: new google.maps.Size(100, 100), // scaled size
-	/*
-    origin: new google.maps.Point(0,0), // origin
-    anchor: new google.maps.Point(0, 0) // anchor */
-};
+
   
   
   var cwebPM = {lat: 20.8635538, lng: -86.9022525};
   var cwebCDMX = {lat: 19.4890817, lng: -99.2057535};
-  var xhtweb = {lat: 20.8698738, lng: -86.9654525};
+
   
   var cweb = '<h3>Comunicación <br/>Web</h3>';
   var cwebPMTXT = '<h3>Comunicación <br/>Web<br/>Puerto Morelos</h3>';
-  var xhtwebTXT = '<h3>XHTWEB</h3>';
+
   
   
   var infowindowCWEB = new google.maps.InfoWindow({
     content: cweb
   });
   
-  var infowindowXHTWEB = new google.maps.InfoWindow({
-    content: xhtwebTXT
-  });
+
   
   var infowindowCWEBPM = new google.maps.InfoWindow({
     content: cwebPMTXT
@@ -84,17 +108,6 @@ var iconXHTWEB = {
 	markCWEBPM.addListener('click', function() {
 		infowindowCWEBPM.open(map, markCWEBPM);
 	});
-	
-  var markXHTWEB = new google.maps.Marker({
-    position: xhtweb,
-    map: map,
-    title: 'XHTWEB', 
-	icon: iconXHTWEB
-  });
-    
-	markXHTWEB.addListener('click', function() {
-		infowindowXHTWEB.open(map, markXHTWEB);
-	});
 
 }
     </script>
@@ -105,6 +118,7 @@ var iconXHTWEB = {
     -->
     <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAQcBMevHC_wfrYNyzr2RKv5mrPiBhtGt4&callback=initMap">
     </script>
+	
   </body>
 </html>
 
